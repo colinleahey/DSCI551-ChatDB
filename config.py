a29156d90bc98b284d6c8de60486e0b849842c08
colinleahey <113860927+colinleahey@users.noreply.github.com>
2025-08-07 16:43:57 -0700

    Update config.py

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ import os
 
 # Replace these default strings or set the corresponding environment variables:
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "key-here")
-MONGO_URI = os.getenv("MONGODB_URI",    "mongodb+srv://cleahey:Yonderelk89%2A@dsci551-mongodb.ofzw9.mongodb.net/?retryWrites=true&w=majority")
+MONGO_URI = os.getenv("MONGODB_URI", "mongo-uri-here")
